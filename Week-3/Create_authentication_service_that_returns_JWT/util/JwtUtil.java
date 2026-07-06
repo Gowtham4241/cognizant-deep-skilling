a15d7f118aaea079ccc6b6c06b6bd624cf98a60a
Gowtham4241 <23501a4241@pvpsit.ac.in>
2026-07-06 23:40:44 +0530

    Add files via upload

---
+++ Week-3/Create_authentication_service_that_returns_JWT/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.cognizant.authenticationservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final SecretKey KEY =
+            Keys.hmacShaKeyFor("ThisIsASecretKeyForJwtGeneration123456".getBytes());
+
+    public static String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1200000))
+                .signWith(KEY)
+                .compact();
+    }
+}
